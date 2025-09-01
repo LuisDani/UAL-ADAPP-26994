1b1b53e56fefc15e3d98a82a7eb832e5a484451d
LuisDani <spartandanii117@gmail.com>
2025-09-01 10:11:19 -0600

    Entregable 6 DataFrame result

--- UAL-ADAPP/rapidfuzz_tables_app.py
+++ UAL-ADAPP/rapidfuzz_tables_app.py
@@ -1,4 +1,5 @@
 from fuzzFunctions import execute_dynamic_matching
+import pandas as pd
 
 params_dict = {
     "sourceDatabase": "crm",          
@@ -14,4 +15,5 @@ params_dict = {
 
 resultados = execute_dynamic_matching(params_dict, score_cutoff=70)
 matches_filtrados = [r for r in resultados if r.get('score', 0) > 70]  
-print(matches_filtrados)
+df = pd.DataFrame(matches_filtrados)
+print(df)
